2dd0e9f5fec6e4879b9b90faa16a4ad89ea9c6c0
Rakesh jaiswal <rakeshjaiswal1c@gmail.com>
2023-06-29 20:49:22 +0530

    my portfolio website

---
+++ react-portfolio/src/pages/Contact.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Layout from "../components/Layout";
+
+function Contact() {
+  return (
+    <Layout>
+     <div className='item-center'>
+  <div className='text-2xl my-20 text-center'>
+    <div className='my-2'><strong>Name:</strong> Rakesh Kumar Jaiswal</div>
+    <div className='my-2 mr-12'><strong>Contact:</strong> +91-8989490248</div>
+    <div className='my-2 ml-14'><strong>Email:</strong> rakeshjaiswal1c@gmail.com</div>
+    <div className='ml-20'><strong>Address:</strong> H.N.69 Jharna Ghorawari Dist.Chhindwara (M.P.) 480555</div>
+  </div>
+</div>
+
+    </Layout>
+  );
+}
+
+export default Contact;
---
+++ react-portfolio/src/resources/projects.js
@@ -0,0 +1,46 @@
+const projectsData = [
+    {
+      title: "CYBSHOP",
+      image: "./ecommerce.svg",
+      description:
+        "An ecommerce application developed using react , node , redux and mongodb",
+      link: "/",
+    },
+    {
+      title: "CYBPIZZA",
+      image: "./pizzas.svg",
+      description:
+        "An ecommerce application developed using react , node , redux and mongodb",
+      link: "/",
+    },
+    {
+      title: "CYBROOMS",
+      image: "./hotels.svg",
+      description:
+        "An ecommerce application developed using react , node , redux and mongodb",
+      link: "/",
+    },
+    {
+      title: "CYBJOBS",
+      image: "./jobs.svg",
+      description:
+        "An ecommerce application developed using react , node , redux and mongodb",
+      link: "/",
+    },
+    {
+      title: "CYBGRAM",
+      image: "./socialmedia.svg",
+      description:
+        "An ecommerce application developed using react , node , redux and mongodb",
+      link: "/",
+    },
+    {
+      title: "CYBCARS",
+      image: "./cars.svg",
+      description:
+        "An ecommerce application developed using react , node , redux and mongodb",
+      link: "/",
+    },
+  ];
+  
+  export default projectsData;
